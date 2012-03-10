3d78f4617c1c8bba96c974dbc783165032a3e44e
C Shoe <schomaker.c@gmail.com>
2012-03-10 13:47:26 -0800

    RegEx FTW

--- connect4.py
+++ connect4.py
@@ -1,93 +1,20 @@
-import re
-import sys
-
-def get_vertical_strings(board):
-    """ Get string representation of columns """
-    column_strings = []
-    for column in range(0, 7):
-        column_string = ''
-        for row in range(0, 6):
-            column_string += board[row][column]
-        column_strings.append(''.join(column_string))
-    # print column_strings
-    return column_strings
-
-
-def get_horizontal_strings(board):
-    """ Get string representation of rows """
-    row_strings = []
-    for row in board:
-        row_strings.append(''.join(row))
-    # print row_strings
-    return row_strings
-
-
-def get_diagonal_strings(board):
-    """
-    Get string representation of diagonal areas where a connect 4
-    is possible.
-
-    """
-    d_strings = []
-
-
-    d_strings = []
-    for column in range(0, 7):
-        row = 0
-        _string = ''
-        try:
-            next_char = board[row][column]
-        except IndexError:
-            d_strings.extend(''.join(_string))
-        else:
-            _string += next_char
-            row += 1
-
-    for column in reversed(range(0, 7)):
-        row = 0
-        _string = ''
-        try:
-            next_char = board[row][column]
-        except IndexError:
-            d_strings.extend(''.join(_string))
-        else:
-            _string += next_char
-            row += 1
-    print d_strings
-    return d_strings
-
-
-def flatten_board(board):
-    """
-    Flatten JSON representation of a board. Return
-    a list of 25 string that represent all ways that
-    a connect 4 can happen.
-
-    """
-    strings = []
-    strings.extend(get_vertical_strings(board))
-    strings.extend(get_horizontal_strings(board))
-    strings.extend(get_diagonal_strings(board))
-    return strings
-
+import re, sys
 
 def find_winner(board):
-    """ Find a winner of the game if there is one """
-    strings = flatten_board(board)
-    x_winner = re.compile('[X]{4}')
-    o_winner = re.compile('[O]{4}')
-    # print strings
-    for string in strings:
-        if string is not None and string is not []:
-            # print string
-            if x_winner.search(string) is not None:
-                return 'Winner: X'
-            elif o_winner.search(string) is not None:
-                return 'Winner: 0'
+    column_count = len(board[0])
+    winners = ['X', 'O']
+    for winner in winners:
+        h_regex = '{}{{4}}'.format(winner)
+        for row in board:
+            if re.search(h_regex, ''.join(row)) is not None:
+                return 'Winner: {}'.format(winner)
+        board_string = ''.join([''.join(row) for row in board])
+        d_v_regexes = ['({0[0]}[XO.]{{{0[1]}}}){{3}}{0[0]}'.\
+            format((winner, column_count+x)) for x in [-2, -1, 0]]
+        for regex in d_v_regexes:
+            if re.search(regex, board_string) is not None:
+                return 'Winner: {}'.format(winner)
     return 'No winner.'
 
-
 if __name__ == '__main__':
-    board = sys.stdin.read()
-    print find_winner(eval(board))
-
+    print find_winner(eval(sys.stdin.read()))
