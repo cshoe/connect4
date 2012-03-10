6dccb129168260a9d98429b40655925a7dad6ae8
C Shoe <schomaker.c@gmail.com>
2012-03-10 14:19:10 -0800

    Keep new lines. Use to detect horizontal wins with regex.

--- connect4.py
+++ connect4.py
@@ -3,18 +3,14 @@ import sys
 
 
 def find_winner(board):
-    c_cnt = len(board[0])
-    winners = ['X', 'O']
-    for winner in winners:
-        h_regex = '{}{{4}}'.format(winner)
-        for row in board:
-            if re.search(h_regex, ''.join(row)) is not None:
-                return 'Winner: {}'.format(winner)
-        board_string = ''.join([''.join(row) for row in board])
-        _regex = '({0[0]}[XO.]{{{0[1]}}}){{3}}{0[0]}'
-        d_v_regexes = [_regex.format((winner, c_cnt + x)) for x in [-2, -1, 0]]
+    for winner in ['X', 'O']:
+        board_string = '\n'.join([''.join(row) for row in board])
+        _regex = '({0[0]}[XO.\n]{{{0[1]}}}){{3}}{0[0]}'
+        c_cnt = len(board[0])
+        d_v_regexes = [_regex.format((winner, c_cnt + x)) for x in [-1, 0, 1]]
+        d_v_regexes.append('{}{{4}}(.)*\n'.format(winner))
         for regex in d_v_regexes:
-            if re.search(regex, board_string) is not None:
+            if re.search(regex, board_string, re.DOTALL) is not None:
                 return 'Winner: {}'.format(winner)
     return 'No winner.'
 
