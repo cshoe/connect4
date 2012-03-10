b1d0baa23212bdf81ca8da2871c353964af3f838
C Shoe <schomaker.c@gmail.com>
2012-03-10 13:52:28 -0800

    PEP-8, yo

--- connect4.py
+++ connect4.py
@@ -1,7 +1,9 @@
-import re, sys
+import re
+import sys
+
 
 def find_winner(board):
-    column_count = len(board[0])
+    c_cnt = len(board[0])
     winners = ['X', 'O']
     for winner in winners:
         h_regex = '{}{{4}}'.format(winner)
@@ -9,8 +11,8 @@ def find_winner(board):
             if re.search(h_regex, ''.join(row)) is not None:
                 return 'Winner: {}'.format(winner)
         board_string = ''.join([''.join(row) for row in board])
-        d_v_regexes = ['({0[0]}[XO.]{{{0[1]}}}){{3}}{0[0]}'.\
-            format((winner, column_count+x)) for x in [-2, -1, 0]]
+        _regex = '({0[0]}[XO.]{{{0[1]}}}){{3}}{0[0]}'
+        d_v_regexes = [_regex.format((winner, c_cnt + x)) for x in [-2, -1, 0]]
         for regex in d_v_regexes:
             if re.search(regex, board_string) is not None:
                 return 'Winner: {}'.format(winner)
